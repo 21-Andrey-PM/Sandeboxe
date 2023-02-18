f44480154d42a65c940a268d7e91f18821230133
Andrey Kovalev <akovalev1789@gmail.com>
2023-02-18 11:41:02 +0300

    Adding

---
+++ HomeWork4.py
@@ -0,0 +1,27 @@
+from  import add, sub, mul, div
+class Calculator:
+    def __init__(self):
+        self.main()
+    def main(self):
+        print("Это калькулятор")
+        while True:
+            num1 = int(input("Введите первое число: "))
+            num2 = int(input("Введите второе число: "))
+            choice = int(input("Выберите действие 1:+, 2:-, 3:*, 4:/"))
+            match choice:
+                case 0:
+                    print("Для завершения нажмите Enter")
+                    input()
+                    break
+                case 1:
+                    print(add(num1, num2))
+                case 2:
+                    print(sub(num1,num2))
+                case 3:
+                    print(mul(num1, num2))
+                case 4:
+                    print(div(num1, num2))
+                case 5:
+                    print("Неверный выбор")
+
+obj = Calculator()
